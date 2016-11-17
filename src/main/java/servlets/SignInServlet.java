7fae1e5c379eb9530b6150cc42363ffdf0f2351c
Dmytro Herasymenko <mcdimamc@gmail.com>
2016-11-17 16:09:18 +0200

    Second lesson. Add registration, authorization and logout pages.

--- src/main/java/servlets/SignInServlet.java
+++ src/main/java/servlets/SignInServlet.java
@@ -1,7 +1,16 @@
 package servlets;
 
+import account.AccountService;
+
+import javax.servlet.http.HttpServlet;
+
 /**
  * Created by dima on 17.11.16.
  */
-public class SignInServlet {
+public class SignInServlet extends HttpServlet{
+    public final AccountService accountService;
+
+    public SignInServlet(AccountService accountService) {
+        this.accountService = accountService;
+    }
 }
